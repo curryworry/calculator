f5488c62a3e4a56104cf5ad0773e643dfc600af5
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-11-02 22:15:12 +1300

    Added error messages when numbers are longer than screen allows

--- scripts.js
+++ scripts.js
@@ -50,14 +50,31 @@ function digitClicked(e){
         display.textContent="";
         clearScreen = false;
     }
+    if(firstValue.toString().length>15||isNaN(firstValue)){
+        displayRestartMessage();
+    }
     display.textContent += e.target.textContent; 
+    if(display.textContent.length>15){
+        displayRestartMessage();
+    }
+    
+}
+
+function displayRestartMessage(){
+    display.textContent="Number too long. CLEAR to restart.";
 }
 
 function displayResult(){
     display.textContent = result;
+    if(display.textContent.length>15){
+        displayRestartMessage();
+    }
 }
 
 function operatorClicked(e){
+    if(display.textContent.includes("restart")){
+        displayRestartMessage();
+    }
     if(operator==""||operator=="="||lastClick=="operator"){
         operator = e.target.textContent;
         firstValue = parseInt(display.textContent);
@@ -66,7 +83,7 @@ function operatorClicked(e){
     else{
         secondValue = parseInt(display.textContent);
         result = operate(operator,firstValue,secondValue);
-        result = Math.round((result + Number.EPSILON) * 10000) / 10000;
+        result = Math.round((result + Number.EPSILON) * 10000) / 10000; //rounds to 4 decimal places
         displayResult();
         operator = e.target.textContent;
         firstValue = result;
@@ -96,7 +113,12 @@ function multiply(a,b){
 }
 
 function divide(a,b){
+    if(b==0){
+        display.textContent="Don't be mean. CLEAR to restart";
+    }
+    else{
     return a / b;
+    }
 }
 
 function operate(operator,a,b){
